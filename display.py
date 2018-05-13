36237e19462297a31fc025511c04d101a355cfd9
Matt Warman <mwarman@kabbage.com>
2018-05-13 10:45:15 -0400

    Fix bad ref, clean up

--- display.py
+++ display.py
@@ -1,6 +1,6 @@
 import RPi_I2C_driver
 
-custom_chars = [
+spinner_chars = [
         # Char 0 - "-"
         [ 0x00, 0x00, 0x00, 0x00, 0x1f, 0x00, 0x00, 0x00 ],
         # Char 1 - "\"
@@ -13,22 +13,22 @@ custom_chars = [
 
 class Display:
     display = None
-    display_status_current_indicator = 0
+    current_spinner_char = 0
 
     def __init__(self):
         self.display = RPi_I2C_driver.lcd()
         self.update("Loading,", 1)
         self.update("Please wait...", 2)
 
-        self.display.lcd_load_custom_chars(custom_chars)
+        self.display.lcd_load_custom_chars(spinner_chars)
 
     def update(self, text, line):
-        # Update the status indicator
-        if self.display_status_current_indicator >= len(self.display_status_indicators):
-            self.display_status_current_indicator = 0
+        # Update the spinner
+        if self.current_spinner_char >= len(spinner_chars):
+            self.current_spinner_char = 0
         self.display.lcd_display_string("Data:", 1)
-        self.display.lcd_display_string_pos(chr(self.display_status_current_indicator), 1, 19)
-        self.display_status_current_indicator += 1
+        self.display.lcd_display_string_pos(chr(self.current_spinner_char), 1, 19)
+        self.current_spinner_char += 1
         
         self.display.lcd_display_string(text, line)
 
