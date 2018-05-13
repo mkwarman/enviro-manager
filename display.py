1453d06278015df58772ef3c9daf265ac7d183e6
Matt Warman <mwarman@kabbage.com>
2018-05-13 10:37:16 -0400

    Define custom chars explicitly

--- display.py
+++ display.py
@@ -1,8 +1,18 @@
 import RPi_I2C_driver
 
+custom_chars = [
+        # Char 0 - "-"
+        [ 0x00, 0x00, 0x00, 0x00, 0x1f, 0x00, 0x00, 0x00 ],
+        # Char 1 - "\"
+        [ 0x10, 0x08, 0x08, 0x04, 0x04, 0x02, 0x02, 0x01 ],
+        # Char 2 - "|"
+        [ 0x04, 0x04, 0x04, 0x04, 0x04, 0x04, 0x04, 0x04 ],
+        # Char 3 - "/"
+        [ 0x00, 0x10, 0x08, 0x04, 0x02, 0x01, 0x00, 0x00 ],
+]
+
 class Display:
     display = None
-    display_status_indicators = ["-", "\\", "|", "/"]
     display_status_current_indicator = 0
 
     def __init__(self):
@@ -10,16 +20,17 @@ class Display:
         self.update("Loading,", 1)
         self.update("Please wait...", 2)
 
+        self.display.lcd_load_custom_chars(custom_chars)
+
     def update(self, text, line):
         # Update the status indicator
         if self.display_status_current_indicator >= len(self.display_status_indicators):
             self.display_status_current_indicator = 0
-        header = "Data:              " + self.display_status_indicators[self.display_status_current_indicator]
-        self.display.lcd_display_string(header, 1)
-
-        self.display.lcd_display_string(text, line)
-
+        self.display.lcd_display_string("Data:", 1)
+        self.display.lcd_display_string_pos(chr(self.display_status_current_indicator), 1, 19)
         self.display_status_current_indicator += 1
+        
+        self.display.lcd_display_string(text, line)
 
     def off(self):
         self.display.lcd_clear()
