29f980f9832a8e7abe3e290fb39579abcd15b8b3
Matt Warman <mwarman@kabbage.com>
2018-05-13 11:41:33 -0400

    h -> H

--- enviro-manager.py
+++ enviro-manager.py
@@ -239,11 +239,11 @@ def poll_sensor_loop():
 @app.route('/index')
 def index():
     return "<p><h1>Sensor Status:</h1>" \
-            + "<h2>Probe temp: {0:0.2f} (target: {1}F) --- Last updated: {2:%h:%M:%S %Y-%m-%d}</br>".format(probe_temp, MAT_TEMP_TARGET, probe.last_updated) \
+            + "<h2>Probe temp: {0:0.2f} (target: {1}F) --- Last updated: {2:%H:%M:%S %Y-%m-%d}</br>".format(probe_temp, MAT_TEMP_TARGET, probe.last_updated) \
             + (("<strong>" + probe.disabled_string + "</strong>") if probe.disabled else "") \
-            + "Sensor1 (temperature): temp={0:0.2f}F hum={1:0.2f}% (target temperature: {2}F) --- Last updated: {3:%h:%M:%S %Y-%m-%d}</br>".format(sensor_values[0].get('temp'), sensor_values[0].get('hum'), AMBIENT_TEMP_TARGET, dht1_temp.last_updated) \
+            + "Sensor1 (temperature): temp={0:0.2f}F hum={1:0.2f}% (target temperature: {2}F) --- Last updated: {3:%H:%M:%S %Y-%m-%d}</br>".format(sensor_values[0].get('temp'), sensor_values[0].get('hum'), AMBIENT_TEMP_TARGET, dht1_temp.last_updated) \
             + (("<strong>" + dht1_temp.disabled_string + "</strong>") if dht1_temp.disabled else "") \
-            + "Sensor2 (humidity): temp={0:0.2f}F hum={1:0.2f}% (target humidity range: {2}% to {3}%) --- Last updated: {4:%h:%M:%S %Y-%m-%d}</h2></p>".format(sensor_values[1].get('temp'), sensor_values[1].get('hum'), HUMIDITY_LOWER_BOUND, HUMIDITY_UPPER_BOUND, dht2_humidity.last_updated) \
+            + "Sensor2 (humidity): temp={0:0.2f}F hum={1:0.2f}% (target humidity range: {2}% to {3}%) --- Last updated: {4:%H:%M:%S %Y-%m-%d}</h2></p>".format(sensor_values[1].get('temp'), sensor_values[1].get('hum'), HUMIDITY_LOWER_BOUND, HUMIDITY_UPPER_BOUND, dht2_humidity.last_updated) \
             + (("<strong>" + dht2_humidity.disabled_string + "</strong>") if dht2_humidity.disabled else "") \
             + "<p><h1>Appliance Status:</h1>" \
             + "<h2>Mat enabled: {0} --- Current status: {1} --- Duty Cycle: {2} (about {3:0.2f}%)</br>".format(mat_enabled, ("on" if gpio.mat_state == ON else "off"), str(mat.duty_cycle), mat.get_duty_cycle_percentage()) \
