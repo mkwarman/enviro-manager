6c2c4c62c7a27b04b12d5c4c76f187320cc8bd55
Matt Warman <mwarman@kabbage.com>
2018-07-08 16:08:38 -0400

    Add logic to use a config file

--- enviro-manager.py
+++ enviro-manager.py
@@ -15,6 +15,7 @@ import serial
 import requests
 import os
 import eventlet
+import settings
 
 eventlet.monkey_patch()
 
@@ -23,23 +24,7 @@ app = Flask(__name__)
 app.config['DEBUG'] = False
 
 socketio = SocketIO(app)
-
-# Environment vars
-PUSHBULLET_TOKEN = os.environ.get('PUSHBULLET_TOKEN') or None
-
-PUSHBULLET_PUSH_URL = 'https://api.pushbullet.com/v2/pushes'
-PROBE_DIRECTORY = "/sys/bus/w1/devices/28-0317030193ff/w1_slave"
-
-MAT_TEMP_LOWER_BOUND = 90
-MAT_TEMP_TARGET = 93
-MAT_TEMP_UPPER_BOUND = 96
-MAT_TEMP_DANGER_ZONE = 100
-AMBIENT_TEMP_LOWER_BOUND = 85
-AMBIENT_TEMP_TARGET = 90
-AMBIENT_TEMP_UPPER_BOUND = 95
-AMBIENT_TEMP_DANGER_ZONE = 100
-HUMIDITY_UPPER_BOUND = 65
-HUMIDITY_LOWER_BOUND = 45
+config = settings.Config()
 
 MAT_SERIAL_IDENTIFIER = "M"
 LIGHT_SERIAL_IDENTIFIER = "L"
@@ -48,25 +33,17 @@ ZERO_CROSS_IDENTIFIER = "Z"
 ON = 1
 OFF = 0
 
-CONCURRENT_READ_FAILURE_ALERT_THRESHOLD = 10
-
 MAT_TEMPERATURE_APPROACH_DELTA_LIMIT = 0.12
 AMBIENT_TEMPERATURE_APPROACH_DELTA_LIMIT = 0.2
 
-SAVE_FILE_NAME = '.enviro_manager_pickle'
-
 display = Display()
-probe = Probe(PROBE_DIRECTORY, 'Probe')
+probe = Probe(config.get('probe', 'READ_DIRECTORY'), 'Probe')
 dht1_temp = DHT22(gpio.DHT_SENSOR1_PIN, 1, 'Sensor1 (temperature)')
 dht2_humidity = DHT22(gpio.DHT_SENSOR2_PIN, 2, 'Sensor2 (humidity)')
 mat = DutyCycle("Mat", MAT_SERIAL_IDENTIFIER)
 light = DutyCycle("Light", LIGHT_SERIAL_IDENTIFIER)
 
 light.duty_cycle_delta = 10
-
-mat_enabled = True
-light_enabled = True # These two are essentially linked
-fogger_enabled = True # at the moment. Disabling one changes nothing
 
 serialConnection = serial.Serial('/dev/serial0', 9600)
 
@@ -93,7 +70,7 @@ args = parser.parse_args()
 if args.gpio_disabled:
     gpio.enabled(False)
 if args.load_file:
-    with open(SAVE_FILE_NAME, 'rb') as f:
+    with open(config.get('save_file', 'NAME'), 'rb') as f:
         light = pickle.load(f)
         mat = pickle.load(f)
     print("Sending loaded values...")
@@ -102,8 +79,8 @@ if args.load_file:
 
 def send_alert(title, body):
     payload = '{"body": \"' + body + '\", "title": \"' + title + '\", "type": "note"}'
-    headers = {'Access-Token': PUSHBULLET_TOKEN, 'Content-Type': 'application/json'}
-    url = PUSHBULLET_PUSH_URL
+    headers = {'Access-Token': config.get('external', 'PUSHBULLET_TOKEN'), 'Content-Type': 'application/json'}
+    url = config.get('external', 'PUSHBULLET_URL')
     print("Attempting to send alert with title: \"" + title + "\" and body:\n" + body)
     r = requests.post(url, data=payload, headers=headers)
     print("response: " + r.text)
@@ -123,7 +100,7 @@ def run_probe(probe):
     display.update(display_string, 2)
 
     if not temp:
-        if probe.concurrent_failures == CONCURRENT_READ_FAILURE_ALERT_THRESHOLD:
+        if probe.concurrent_failures == config.getint('alert', 'CONCURRENT_READ_FAILURE_ALERT_THRESHOLD'):
             gpio.set_mat(OFF)
             send_alert("Read Error", str(probe.concurrent_failures) + " probe read failures in a row")
 
@@ -137,9 +114,15 @@ def run_probe(probe):
     previous_temp = probe_temp
     probe_temp = temp
 
-    if temp < MAT_TEMP_TARGET:
+    section = 'mat_bounds'
+    lower_bound = config.getint(section, 'LOWER_BOUND')
+    target      = config.getint(section, 'TARGET')
+    upper_bound = config.getint(section, 'UPPER_BOUND')
+    danger_zone = config.getint(section, 'DANGER_ZONE')
+
+    if temp < target:
         gpio.set_mat(ON)
-        if temp < MAT_TEMP_LOWER_BOUND:
+        if temp < lower_bound:
             print("Max mat duty cycle")
             mat.max_duty_cycle(serialConnection)
         elif (previous_temp > temp):
@@ -148,10 +131,10 @@ def run_probe(probe):
         elif (temp - previous_temp > MAT_TEMPERATURE_APPROACH_DELTA_LIMIT):
             print("Decrease mat duty cycle due to approach speed")
             mat.decrease_duty_cycle(serialConnection)
-    elif temp > MAT_TEMP_TARGET:
-        if temp > MAT_TEMP_UPPER_BOUND:
+    elif temp > target:
+        if temp > upper_bound:
             gpio.set_mat(OFF)
-            if temp > MAT_TEMP_DANGER_ZONE:
+            if temp > danger_zone:
                 send_alert("Temperature Alert", "Mat temperature too high: " + str(temp))
         # Dont mess with duty cycle if state is off
         elif (gpio.mat_state == ON):
@@ -173,7 +156,7 @@ def run_dht_temp(dht):
             'hum': dht_hum
         }
     else:
-        if dht.concurrent_failures == CONCURRENT_READ_FAILURE_ALERT_THRESHOLD:
+        if dht.concurrent_failures == config.getint('alert', 'CONCURRENT_READ_FAILURE_ALERT_THRESHOLD'):
             gpio.set_light(OFF)
             send_alert("Read Error", str(dht.concurrent_failures) + " temp DHT read failures in a row")
         print("DHT temp sensor didn't return usable data")
@@ -186,9 +169,15 @@ def run_dht_temp(dht):
     previous_ambient_temp = ambient_temp
     ambient_temp = dht_temp
 
-    if ambient_temp < AMBIENT_TEMP_TARGET:
+    section = 'ambient_bounds'
+    lower_bound = config.getint(section, 'LOWER_BOUND')
+    target      = config.getint(section, 'TARGET')
+    upper_bound = config.getint(section, 'UPPER_BOUND')
+    danger_zone = config.getint(section, 'DANGER_ZONE')
+    
+    if ambient_temp < target:
         gpio.set_light(ON)
-        if ambient_temp < AMBIENT_TEMP_LOWER_BOUND:
+        if ambient_temp < lower_bound:
             print("Max light duty cycle")
             light.max_duty_cycle(serialConnection)
         # If temperature falling
@@ -198,10 +187,10 @@ def run_dht_temp(dht):
         elif (ambient_temp - previous_ambient_temp > AMBIENT_TEMPERATURE_APPROACH_DELTA_LIMIT):
             print("Decrease light duty cycle due to approach speed")
             light.decrease_duty_cycle(serialConnection)
-    elif ambient_temp > AMBIENT_TEMP_TARGET:
-        if ambient_temp > AMBIENT_TEMP_UPPER_BOUND:
+    elif ambient_temp > target:
+        if ambient_temp > upper_bound:
             gpio.set_light(OFF)
-            if ambient_temp > AMBIENT_TEMP_DANGER_ZONE:
+            if ambient_temp > danger_zone:
                 send_alert("Temperature Alert", "Ambient temperature too high: " + str(ambient_temp))
         # Dont mess with duty cycle if state is off
         elif (gpio.light_state == ON):
@@ -229,29 +218,33 @@ def run_dht_humidity(dht):
     if display_string:
         display.update(display_string, dht.number + 2)
 
-    if dht_hum < HUMIDITY_LOWER_BOUND:
+    section = 'humidity_bounds'
+    lower_bound = config.getint(section, 'LOWER_BOUND')
+    upper_bound = config.getint(section, 'UPPER_BOUND')
+
+    if dht_hum < lower_bound:
         gpio.set_fogger(ON)
         print("Turn on fogger")
-    elif dht_hum > HUMIDITY_UPPER_BOUND:
+    elif dht_hum > upper_bound:
         print("Turn off fogger")
         gpio.set_fogger(OFF)
 
 def poll_sensor_loop():
     while poll_sensors:
         try:
-            if (mat_enabled):
+            if (config.getboolean('application_status', 'MAT')):
                 run_probe(probe)
                 socketio.emit('sensor_update',
                     {'sensors': get_sensors_object(), 'appliances': get_appliances_object()},
                     namespace='/live')
             eventlet.sleep(1)
-            if (light_enabled):
+            if (config.getboolean('application_status', 'LIGHT')):
                 run_dht_temp(dht1_temp)
                 socketio.emit('sensor_update',
                     {'sensors': get_sensors_object(), 'appliances': get_appliances_object()},
                     namespace='/live')
             eventlet.sleep(1)
-            if (fogger_enabled):
+            if (config.getboolean('application_status', 'FOGGER')):
                 run_dht_humidity(dht2_humidity)
                 socketio.emit('sensor_update',
                     {'sensors': get_sensors_object(), 'appliances': get_appliances_object()},
@@ -284,11 +277,15 @@ def get_sensors_object():
             .format((now - dht1_temp.last_updated).seconds) if dht1_temp.last_updated else "never"
     dht2_humidity_last_updated = "{0}s ago" \
             .format((now - dht2_humidity.last_updated).seconds) if dht2_humidity.last_updated else "never"
+    mat_target      = config.getint('mat_bounds', 'TARGET')
+    ambient_target  = config.getint('ambient_bounds', 'TARGET')
+    hum_lower       = config.getint('humidity_bounds', 'LOWER')
+    hum_upper       = config.getint('humidity_bounds', 'UPPER')
     sensors = [
         {
             'name': probe.name,
             'temperature': '{0:0.2f}F'.format(probe_temp),
-            'target_temperature': "{0}F".format(MAT_TEMP_TARGET),
+            'target_temperature': "{0}F".format(mat_target),
             'humidity': Markup('<span class="na">N/A</span>'),
             'target_humidity': Markup('<span class="na">N/A</span>'),
             'last_updated': probe_last_updated,
@@ -298,7 +295,7 @@ def get_sensors_object():
         {
             'name': dht1_temp.name,
             'temperature': '{0:0.2f}F'.format(sensor_values[0].get('temp')),
-            'target_temperature': "{0}F".format(AMBIENT_TEMP_TARGET),
+            'target_temperature': "{0}F".format(ambient_target),
             'humidity': '{0:0.2f}%'.format(sensor_values[0].get('hum')),
             'target_humidity': Markup('<span class="na">N/A</span>'),
             'last_updated': dht1_temp_last_updated,
@@ -310,7 +307,7 @@ def get_sensors_object():
             'temperature': '{0:0.2f}F'.format(sensor_values[1].get('temp')),
             'target_temperature': Markup('<span class="na">N/A</span>'),
             'humidity': '{0:0.2f}%'.format(sensor_values[1].get('hum')),
-            'target_humidity': '{0}% to {1}%'.format(AMBIENT_TEMP_LOWER_BOUND, AMBIENT_TEMP_UPPER_BOUND),
+            'target_humidity': '{0}% to {1}%'.format(ambient_lower, ambient_upper),
             'last_updated': dht2_humidity_last_updated,
             'concurrent_failures': dht2_humidity.concurrent_failures,
             'total_failures': dht2_humidity.total_failures
@@ -322,21 +319,21 @@ def get_appliances_object():
     appliances = [
         {
             'name': mat.name,
-            'enabled': mat_enabled,
+            'enabled': config.getboolean('application_status', 'MAT'),
             'status': ("On" if gpio.mat_state == ON else "Off"),
             'duty_cycle': str(mat.duty_cycle),
             'duty_cycle_percentage': '{0:0.2f}%'.format(mat.get_duty_cycle_percentage())
         },
         {
             'name': light.name,
-            'enabled': light_enabled,
+            'enabled': config.getboolean('application_status', 'LIGHT'),
             'status': ("On" if gpio.light_state == ON else "Off"),
             'duty_cycle': str(light.duty_cycle),
             'duty_cycle_percentage': '{0:0.2f}%'.format(light.get_duty_cycle_percentage())
         },
         {
             'name': 'Fogger',
-            'enabled': fogger_enabled,
+            'enabled': config.getboolean('application_status', 'FOGGER'),
             'status': ("On" if gpio.fogger_state == ON else "Off"),
             'duty_cycle': Markup('<span class="na">N/A</span>'),
             'duty_cycle_percentage': Markup('<span class="na">N/A</span>')
@@ -347,7 +344,7 @@ def get_appliances_object():
 def save_state():
     global light
     global mat
-    with open(SAVE_FILE_NAME, 'wb') as f:
+    with open(config.get('save_file', 'NAME'), 'wb') as f:
         pickle.dump(light, f)
         pickle.dump(mat, f)
 
@@ -373,29 +370,32 @@ def index():
             .format(dht1_temp.last_updated, (now - dht1_temp.last_updated).seconds) if dht1_temp.last_updated else "never"
     dht2_humidity_last_updated = "{0:%H:%M:%S %Y-%m-%d} --- {1} seconds ago" \
             .format(dht2_humidity.last_updated, (now - dht2_humidity.last_updated).seconds) if dht2_humidity.last_updated else "never"
-    
+    mat_target      = config.getint('mat_bounds', 'TARGET')
+    ambient_target  = config.getint('ambient_bounds', 'TARGET')
+    hum_lower       = config.getint('humidity_bounds', 'LOWER')
+    hum_upper       = config.getint('humidity_bounds', 'UPPER')
     return "<p><h1>Sensor Status:</h1>" \
-            + "<h2>Probe temp: {0:0.2f} (target: {1}F)</br>".format(probe_temp, MAT_TEMP_TARGET) \
+            + "<h2>Probe temp: {0:0.2f} (target: {1}F)</br>".format(probe_temp, mat_target) \
             + "    Last updated: {0}</br>".format(probe_last_updated) \
             + (("<strong>" + probe.disabled_string + "</strong>") if probe.disabled else "") \
             + "Sensor1 (temperature): temp={0:0.2f}F hum={1:0.2f}% (target temperature: {2}F)</br>" \
-              .format(sensor_values[0].get('temp'), sensor_values[0].get('hum'), AMBIENT_TEMP_TARGET) \
+              .format(sensor_values[0].get('temp'), sensor_values[0].get('hum'), ambient_target) \
             + "    Last updated: {0}</br>".format(dht1_temp_last_updated) \
             + (("<strong>" + dht1_temp.disabled_string + "</strong>") if dht1_temp.disabled else "") \
             + "Sensor2 (humidity): temp={0:0.2f}F hum={1:0.2f}% (target humidity range: {2}% to {3}%)</br>" \
-              .format(sensor_values[1].get('temp'), sensor_values[1].get('hum'), HUMIDITY_LOWER_BOUND, HUMIDITY_UPPER_BOUND) \
+              .format(sensor_values[1].get('temp'), sensor_values[1].get('hum'), hum_lower, hum_upper) \
             + "    Last updated: {0}</br>".format(dht2_humidity_last_updated) \
             + (("<strong>" + dht2_humidity.disabled_string + "</strong>") if dht2_humidity.disabled else "") \
             + "</h2></p>" \
             + "<p><h1>Appliance Status:</h1>" \
             + "<h2>Mat enabled: {0} --- Current status: {1} --- Duty Cycle: {2} (about {3:0.2f}%)</br>" \
-              .format(mat_enabled, ("on" if gpio.mat_state == ON else "off"), \
+              .format(config.getboolean('application_status', 'MAT'), ("on" if gpio.mat_state == ON else "off"), \
                       str(mat.duty_cycle), mat.get_duty_cycle_percentage()) \
             + "Light enabled: {0} --- Current status: {1} --- Duty Cycle: {2} (about {3:0.2f}%)</br>" \
-              .format(light_enabled, ("on" if gpio.light_state == ON else "off"), \
+              .format(config.getboolean('application_status', 'LIGHT'), ("on" if gpio.light_state == ON else "off"), \
                       str(light.duty_cycle), light.get_duty_cycle_percentage()) \
             + "Fogger enabled: {0} --- Current status: {1}</br>" \
-              .format(fogger_enabled, ("on" if gpio.fogger_state == ON else "off")) \
+              .format(config.getboolean('application_status', 'FOGGER'), ("on" if gpio.fogger_state == ON else "off")) \
             + "</h2></p>"
 
 @app.route('/test_alert')
---
+++ settings.py
@@ -0,0 +1,61 @@
+from configparser import *
+
+CONFIG_FILE_NAME = 'config.txt'
+
+class Config(ConfigParser):
+    def __init__(self):
+        super(Config, self).__init__()
+        self.load()
+
+    def get(self, *args, **kwargs):
+        return super(Config, self).get(*args, **kwargs)
+
+    def getboolean(self, *args, **kwargs):
+        return super(Config, self).getboolean(*args, **kwargs)
+    
+    def getint(self, *args, **kwargs):
+        return super(Config, self).getint(*args, **kwargs)
+
+    def getfloat(self, *args, **kwargs):
+        return super(Config, self).getfloat(*args, **kwargs)
+
+    def load(self):
+        try:
+            self.read_file(open(CONFIG_FILE_NAME))
+
+        except (FileNotFoundError, IOError):
+            print('No config file found, generating from defaults')
+            self.create_default_conf()
+
+    def create_default_conf(self):
+        self['application_status'] = {
+                'MAT':      'False',
+                'LIGHT':    'False',
+                'FOGGER':   'False'}
+        self['probe'] = {
+                'READ_DIRECTORY':   ''}
+        self['mat_bounds'] = {
+                'LOWER_BOUND':  '',
+                'TARGET':       '',
+                'UPPER_BOUND':  '',
+                'DANGER_ZONE':  ''}
+        self['ambient_bounds'] = {
+                'LOWER_BOUND':  '',
+                'TARGET':       '',
+                'UPPER_BOUND':  '',
+                'DANGER_ZONE':  '',}
+        self['humidity_bounds'] = {
+                'LOWER_BOUND':  ''}
+                'UPPER_BOUND':  '',
+        self['external'] = {
+                'PUSHBULLET_TOKEN': '',
+                'PUSHBULLET_URL':   ''}
+        self['alert'] = {
+                'CONCURRENT_READ_FAILURE_ALERT_THRESHOLD':  '10'}
+        self['save_file'] = {
+                'NAME': '.enviro_manager_pickle'}
+        self.save_config()
+
+    def save_config(self):
+        with open(CONFIG_FILE_NAME, 'w') as configfile:
+            self.write(configfile
