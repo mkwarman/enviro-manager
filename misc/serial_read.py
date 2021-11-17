8fa181c686599370338344f220a9b05ce036e7b6
Matt Warman <mkwarman@gmail.com>
2021-11-17 18:46:26 -0500

    Clean slate

--- misc/serial_read.py
+++
@@ -1,6 +0,0 @@
-import serial
-arduinoSerialData = serial.Serial('/dev/serial0', 9600)
-while 1:
-    if(arduinoSerialData.inWaiting()>0):
-        myData = arduinoSerialData.readline()
-        print(myData)
